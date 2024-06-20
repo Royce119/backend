dfe71d5161d24c8dfc64a44aa86ac4bd713c56bb
Ahmed Essam <49870229+Royce119@users.noreply.github.com>
2024-06-20 20:54:56 +0300

    Delete src directory

--- src/routes/contact/contact.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const contactRouter = express.Router();
-const { postContactForm } = require("./contact.controller");
-
-contactRouter.post("/contact", postContactForm);
-
-module.exports = contactRouter;
